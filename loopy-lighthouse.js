1b991b6601480007db39a6683576a2dd55e35ff3
Elia Chow <eliacchow@gmail.com>
2023-01-08 14:25:31 -0800

    loopy lighthouse count

---
+++ loopy-lighthouse.js
@@ -0,0 +1,13 @@
+var count = 100;
+while (count <= 200) {
+  if (count % 3 === 0 && count % 4 === 0) {
+    console.log("LoopyLighthouse");
+  } else if (count % 4 === 0) {
+    console.log("Lighthouse");
+  } else if (count % 3 === 0) {
+    console.log("Loopy");
+  } else {
+    console.log(count);
+  }
+  count = count + 1;
+}
